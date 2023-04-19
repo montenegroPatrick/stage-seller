fde90ae9fe4104f0c3359bf105b8fc6c3de14efa
Younes <116138482+Younes-Kch@users.noreply.github.com>
2023-04-19 09:00:03 +0200

    auth-route[middleware]

--- middleware.js
+++ middleware.js
@@ -1,12 +1,54 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(request, res) {
-  const cookie = request.cookies.get("jwt");
-  // if (!cookie) {
-  //   return NextResponse.redirect(new URL("/", request.url));
-  // }
+  const jwt = request.cookies.get("jwt")?.value;
+  const userId = request.cookies.get("user-Id")?.value;
+  const role = request.cookies.get("roleUser")?.value;
+  console.log(jwt);
+  console.log(userId);
+  console.log(role);
+
+  if (request.nextUrl.pathname.startsWith("/companies") && !jwt) {
+    return NextResponse.redirect(new URL("/sign-in", request.url));
+  }
+  if (request.nextUrl.pathname.startsWith("/students") && !jwt) {
+    return NextResponse.redirect(new URL("/sign-in", request.url));
+  }
+  if (
+    request.nextUrl.pathname.startsWith("/companies/lists") &&
+    role === "companies"
+  ) {
+    return NextResponse.redirect(
+      new URL(`/companies/profil/${userId}`, request.url)
+    );
+  }
+  if (
+    request.nextUrl.pathname.startsWith("/students/lists") &&
+    role === "students"
+  ) {
+    return NextResponse.redirect(
+      new URL(`/students/profil/${userId}`, request.url)
+    );
+  }
+  if (
+    request.nextUrl.pathname.startsWith("/companies/profil") &&
+    role === "students"
+  ) {
+    return NextResponse.redirect(
+      new URL(`/students/profil/${userId}`, request.url)
+    );
+  }
+  if (
+    request.nextUrl.pathname.startsWith("/students/profil") &&
+    role === "students"
+  ) {
+    return NextResponse.redirect(
+      new URL(`/students/profil/${userId}`, request.url)
+    );
+  }
 }
 
+
 export const config = {
-  matcher: ["/company/:path*/:path*", "/students/:path*/:path*"],
+  matcher: ["/companies/:path*", "/students/:path*"],
 };
